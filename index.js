438737ea64fd1ee083cd5fa42e56ca3793ef21c2
Cindy Germain <cindymarguerite@gmail.com>
2022-03-07 05:46:20 -0700

    commit tree bc0f9361015ee948f93aadf18f992ffde076a538

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record){
+
+    const winningYear = record.find(record=>record.result===`W`)
+
+    if(winningYear){
+        return winningYear.year
+    }
+
+}
+
+
